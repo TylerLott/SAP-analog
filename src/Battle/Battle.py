d981618cab181cb031f0929e67e86475a8839e35
TylerLott <tylerlott0126@gmail.com>
2021-12-18 13:29:05 -0700

    added to battle and team

---
+++ src/Battle/Battle.py
@@ -0,0 +1,12 @@
+from src.Team.Team import Team
+
+class Battle:
+    def __init__(self, team1: Team, team2: Team):
+        self.turn = 1
+        pass
+
+    def fight(self):
+        pass
+
+    def shop(self):
+        pass
---
+++ src/Team/Team.py
@@ -0,0 +1,5 @@
+
+class Team:
+    def __init__(self):
+        self.health = 10
+        self.friends = []
