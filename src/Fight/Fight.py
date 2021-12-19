1ae4c54387996565215d6efcc8855ac01afe215a
TylerLott <tylerlott0126@gmail.com>
2021-12-19 15:53:45 -0700

    made changes to structure

--- src/Fight/Fight.py
+++ src/Fight/Fight.py
@@ -1,12 +1,9 @@
 from src.Team.Team import Team
 
-class Battle:
+class Fight:
     def __init__(self, team1: Team, team2: Team):
         self.turn = 1
         pass
 
-    def fight(self):
-        pass
-
-    def shop(self):
+    def simulate(self):
         pass
