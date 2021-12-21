7151cc81544e90cba0cf35457ba35a643a4a01cf
TylerLott <tylerlott0126@gmail.com>
2021-12-20 18:45:44 -0700

    placeholder food and effects

---
+++ src/Item/Effect/Effect.py
@@ -0,0 +1,47 @@
+
+from abc import ABC
+
+
+class Effect(ABC):
+    def __init__(self):
+        pass
+
+class BoneEffect(Effect):
+    def __init__(self):
+        pass
+    
+class SplashEffect(Effect):
+    def __init__(self):
+        pass
+
+class SteakEffect(Effect):
+    def __init__(self):
+        pass
+
+class PoisonEffect(Effect):
+    def __init__(self):
+        pass
+
+class WeakEffect(Effect):
+    def __init__(self):
+        pass
+
+class GarlicEffect(Effect):
+    def __init__(self):
+        pass
+
+class MelonEffect(Effect):
+    def __init__(self):
+        pass
+
+class HoneyEffect(Effect):
+    def __init__(self):
+        pass
+
+class ExtraLifeEffect(Effect):
+    def __init__(self):
+        pass
+
+class CoconutEffect(Effect):
+    def __init__(self):
+        pass
