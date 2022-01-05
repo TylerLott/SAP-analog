8ef7f778c3908557f39756256fec1bebd5c4c9dd
TylerLott <tylerlott0126@gmail.com>
2022-01-04 23:11:16 -0700

    state working

--- tests/State_test.py
+++ tests/State_test.py
@@ -14,3 +14,18 @@ class StateTests(unittest.TestCase):
         self.assertEqual(state[-2], 1)
         self.assertEqual(state[-3], 1)
         self.assertEqual(state[-4], 2)
+
+        state = a.getState()
+        self.assertEqual(len(state), 83)
+        self.assertEqual(state[1], 1)
+        self.assertEqual(state[-1], 1)
+        self.assertEqual(state[-2], 1)
+        self.assertEqual(state[-3], 1)
+        self.assertEqual(state[-4], 2)
+
+    def test_noneanimal(self):
+        a = NoneAnimal()
+        state = a.getState()
+        self.assertEqual(state[0], 1)
+        for i in state[1:]:
+            self.assertEqual(i, 0)
